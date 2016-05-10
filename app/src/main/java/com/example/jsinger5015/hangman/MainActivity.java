fd47fd2d7f2f71efc30681f68b07bd45786c0ec0
jsinger5015 <jsinger5015@stu.neisd.net>
2016-05-10 10:29:35 -0500

    Added errors

--- app/src/main/java/com/example/jsinger5015/hangman/MainActivity.java
+++ app/src/main/java/com/example/jsinger5015/hangman/MainActivity.java
@@ -2,8 +2,21 @@ package com.example.jsinger5015.hangman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void buttonA2 (View view){
+    b1 Button button = (Button) findViewById(R.id.b1);
+    button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View Main2Activity) {
+
+        }}
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
