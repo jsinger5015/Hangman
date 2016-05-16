970c085da9c7909b897000c9113df40a96b3d974
jsinger5015 <jsinger5015@stu.neisd.net>
2016-05-16 10:08:25 -0500

    added button and fixed errors

--- app/src/main/java/com/example/jsinger5015/hangman/Main2Activity.java
+++ app/src/main/java/com/example/jsinger5015/hangman/Main2Activity.java
@@ -1,9 +1,12 @@
 package com.example.jsinger5015.hangman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +14,9 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
     }
 
+    public void bMain(View view) {
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
 
+    }
 }
--- app/src/main/java/com/example/jsinger5015/hangman/MainActivity.java
+++ app/src/main/java/com/example/jsinger5015/hangman/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jsinger5015.hangman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,8 +8,8 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     public void buttonA2 (View view){
-    b1 Button button = (Button) findViewById(R.id.b1);
-    button.setOnClickListener(new View.OnClickListener()
+   Intent i= new Intent (getApplicationContext(), Main2Activity.class);
+        startActivity(i);
         }
 
     @Override
