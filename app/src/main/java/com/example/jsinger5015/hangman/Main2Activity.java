3519c2ca0d54cfe357e33f692e22a944b8387903
jsinger5015 <jsinger5015@stu.neisd.net>
2016-05-16 10:26:58 -0500

    Most recent version

--- app/src/main/java/com/example/jsinger5015/hangman/Main2Activity.java
+++ app/src/main/java/com/example/jsinger5015/hangman/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.example.jsinger5015.hangman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -11,6 +13,10 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
     }
 
+    public void bButton (View view){
+        Intent i= new Intent (getApplicationContext(), MainActivity.class);
+        startActivity(i);
+    }
 
     public void addingLetter(){
 
