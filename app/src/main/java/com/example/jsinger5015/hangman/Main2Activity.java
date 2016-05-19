4694fd8e0607ae1934e932f0c426e93531b3d76f
marquitarose <littlestar0204>
2016-05-19 10:19:23 -0500

    lkjg

--- app/src/main/java/com/example/jsinger5015/hangman/Main2Activity.java
+++ app/src/main/java/com/example/jsinger5015/hangman/Main2Activity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -18,7 +19,50 @@ public class Main2Activity extends AppCompatActivity {
         startActivity(i);
     }
 
-    public void addingLetter(){
+    public void addLetter(View view) {
+        TextView ltrs = (TextView) view;
+        ltrs.setVisibility(View.INVISIBLE);
+        //withLetters(ltrs);
 
+    }
+
+    public void withLetters(TextView letter) {
+        if (letInWord(letter)) addLetters();
+        else notThere();
+    }
+
+    public void addLetters() {
+
+    }
+
+    public void notThere() { //add to the hangman
+
+    }
+
+    public boolean letInWord(TextView letter) {
+        String hangWord = theWordUsed(ofWords(), getIndex(ofWords()));
+        return (hangWord.contains(letter.getText()));
+    }
+
+    public static String[] ofWords() {
+         String[] words = {"Stomach", "Head", "Hand", "Drive", "Explain", "Stray", "Path", "Aquire", "Adopt", "Tables", "Backdrop", "Backfire",
+                "Silky", "Moth", "Dumb", "Bearish", "Stupid", "Bimonthly", "Bisulfate", "Love", "Hate", "Like", "Luminous", "Spark", "Fire",
+                "Breathing", "Document", "Computer", "Bystander", "Keyboard", "Righteously", "Waterbed", "Ocean",
+                "Swim", "dive", "Die", "Republicans", "Hacker", "Flowchart", "Time", "Horse", "Dragon", "Cave", "Navy", "Army", "Glad", "Documentarily",
+                "Pants", "Uncopyrightable" };
+
+        return words;
+    }
+
+    public int getIndex(String[] words) {
+        words = ofWords();
+        int index = (int) (Math.random() * (words.length - 1));
+        return index;
+    }
+
+    public String theWordUsed(String[] words, int index) {
+        words = ofWords();
+        index = getIndex(words);
+        return words[index].toLowerCase();
     }
 }
--- app/src/main/java/com/example/jsinger5015/hangman/MainActivity.java
+++ app/src/main/java/com/example/jsinger5015/hangman/MainActivity.java
@@ -4,13 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void buttonA2 (View view){
-   Intent i= new Intent (getApplicationContext(), Main2Activity.class);
+    public void buttonA2(View view) {
+        Intent i = new Intent(getApplicationContext(), Main2Activity.class);
         startActivity(i);
-        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +19,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void addLetter(View view){
-       // TextView ltrs =  (TextView) View.INVISIBLE;
+    public void addLetter(View view) {
+        TextView ltrs = (TextView) view;
+        ltrs.setVisibility(View.GONE);
+        //withLetters(ltrs);
+
     }
+
+    /*public void withLetters(TextView letter) {
+        if (letInWord(letter)) addLetters();
+        else notThere();
+    }
+
+    public void addLetters() {
+
+    }
+
+    public void notThere() { //add to the hangman
+
+    }
+
+    public boolean letInWord(TextView letter) {
+        String hangWord = theWordUsed(ofWords(), getIndex(ofWords()));
+        return (hangWord.contains(letter));
+    }
+
+    public static String[] ofWords() {
+         String[] words = {"Stomach", "Head", "Hand", "Drive", "Explain", "Stray", "Path", "Aquire", "Adopt", "Tables", "Backdrop", "Backfire",
+                "Silky", "Moth", "Dumb", "Bearish", "Stupid", "Bimonthly", "Bisulfate", "Love", "Hate", "Like", "Luminous", "Spark", "Fire",
+                "Breathing", "Document", "Computer", "Bystander", "Keyboard", "Righteously", "Waterbed", "Ocean",
+                "Swim", "dive", "Die", "Republicans", "Hacker", "Flowchart", "Time", "Horse", "Dragon", "Cave", "Navy", "Army", "Glad", "Documentarily",
+                "Pants", "Uncopyrightable" };
+
+        return words;
+    }
+
+    public int getIndex(String[] words) {
+        words = ofWords();
+        int index = (int) (Math.random() * (words.length - 1));
+        return index;
+    }
+
+    public String theWordUsed(String[] words, int index) {
+        words = ofWords();
+        index = getIndex(words);
+        return words[index].toLowerCase();
+    }*/
 }
