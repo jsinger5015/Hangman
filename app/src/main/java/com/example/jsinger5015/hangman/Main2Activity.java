29189f57f0f192af472d3c212d43cd18f360d550
marquitarose <littlestar0204>
2016-05-27 09:31:15 -0500

    lines method added

--- app/src/main/java/com/example/jsinger5015/hangman/Main2Activity.java
+++ app/src/main/java/com/example/jsinger5015/hangman/Main2Activity.java
@@ -1,45 +1,71 @@
 package com.example.jsinger5015.hangman;
 
-import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+
 public class Main2Activity extends AppCompatActivity {
+    static String[] words = {"Stomach", "Head", "Hand", "Drive", "Explain", "Stray", "Path", "Aquire", "Adopt", "Tables", "Backdrop", "Backfire",
+            "Silky", "Moth", "Dumb", "Bearish", "Stupid", "Bimonthly", "Bisulfate", "Love", "Hate", "Like", "Luminous", "Spark", "Fire",
+            "Breathing", "Document", "Computer", "Bystander", "Keyboard", "Righteously", "Waterbed", "Ocean",
+            "Swim", "dive", "Die", "Republicans", "Hacker", "Flowchart", "Time", "Horse", "Dragon", "Cave", "Navy", "Army", "Glad", "Documentarily",
+            "Pants", "Uncopyrightable", "Chair", "Mouse", "Ambidextrously", "Answer", "Picture", "Stem", "Shoe",
+            "Plant", "Watch", "Lymph", "Guitar", "Trash", "Magnet", "Bored", "Neck", "Subordinately", "Consumable", "Ownership", "Police", "Unproblematic",
+            "Loser", "Serial","Bankruptcy" };
+
+    TextView ts;
+
+    static int index = getIndex();
+    static String usedWord = theWordUsed();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-    }
+        ts = (TextView)findViewById(R.id.ts);
+        lines();
 
-    public void bButton (View view){
-        Intent i= new Intent (getApplicationContext(), MainActivity.class);
-        startActivity(i);
     }
 
     public void addLetter(View view) {
         TextView ltrs = (TextView) view;
         ltrs.setVisibility(View.INVISIBLE);
         withLetters(ltrs);
-
     }
 
     public void withLetters(TextView letter) {
-        lines(ofWords());
-        if (letInWord(letter)) addLetters();
-        else notThere();
+        if (letInWord(letter)){addLetters(letter);}
+        else
+        {notThere();}
     }
 
 
-    public void lines(String[] words){
-        words = ofWords();
-
+    public void lines(){
+        TextView daWord = (TextView) findViewById(R.id.textView1);
+        daWord.setText(numLines());
+        TextView txt1 = new TextView(Main2Activity.this);
     }
 
+    public String numLines(){
+        int numLet = usedWord.length();
+        String returning = "";
 
-    public void addLetters() {
+        for (int x = 0; x< numLet; x++){
+            returning += " _";
+            System.out.println(usedWord.toUpperCase());
+        }
+        return returning;
+    }
+
+    public void addLetters(TextView let) {
+        char wantedLet = let.toString().charAt(0);
+        int location = words[index].indexOf(wantedLet);
+        TextView zaWord = (TextView) findViewById(R.id.textView1);
+        String editor = zaWord.getText().toString().substring(0, location*2) +  wantedLet + zaWord.getText().toString().substring((location*2) +1);
+        zaWord.setText(editor);
 
     }
 
@@ -48,31 +74,21 @@ public class Main2Activity extends AppCompatActivity {
     }
 
     public boolean letInWord(TextView letter) {
-        String hangWord = theWordUsed(ofWords(), getIndex(ofWords()));
-        return (hangWord.contains(letter.getText()));
+        System.out.println(usedWord);
+        System.out.println(letter.getText());
+        return (usedWord.contains(letter.getText().toString()));
     }
 
-    public static String[] ofWords() {
-         String[] words = {"Stomach", "Head", "Hand", "Drive", "Explain", "Stray", "Path", "Aquire", "Adopt", "Tables", "Backdrop", "Backfire",
-                "Silky", "Moth", "Dumb", "Bearish", "Stupid", "Bimonthly", "Bisulfate", "Love", "Hate", "Like", "Luminous", "Spark", "Fire",
-                "Breathing", "Document", "Computer", "Bystander", "Keyboard", "Righteously", "Waterbed", "Ocean",
-                "Swim", "dive", "Die", "Republicans", "Hacker", "Flowchart", "Time", "Horse", "Dragon", "Cave", "Navy", "Army", "Glad", "Documentarily",
-                "Pants", "Uncopyrightable", "Chair", "Mouse", "Ambidextrously", "Answer", "Picture", "Stem", "Shoe",
-                "Plant", "Watch", "Lymph", "Guitar", "Trash", "Magnet", "Bored", "Neck", "Subordinately", "Consumable", "Ownership", "Police", "Unproblematic",
-                 "Loser", "Serial","Bankruptcy" };
-
-        return words;
+    public static int getIndex() {
+        return (int) (Math.random() * (words.length - 1));
     }
 
-    public int getIndex(String[] words) {
-        words = ofWords();
-        int index = (int) (Math.random() * (words.length - 1));
-        return index;
+    public static String theWordUsed() {
+        return words[index].toUpperCase();
     }
 
-    public String theWordUsed(String[] words, int index) {
-        words = ofWords();
-        index = getIndex(words);
-        return words[index].toLowerCase();
-    }
+
+
 }
+
+
